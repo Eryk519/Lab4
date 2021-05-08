e57755c70a43bd062d84b6c982ecf22887c48a9f
Eryk519 <74097448+Eryk519@users.noreply.github.com>
2021-05-08 13:41:52 +0200

    Update main.py
    Uzupełnienie zadań

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def numpyList(n):
 print(numpyList(10))
 
 #ZAD4
-
+ def generuj(liczba, ilosc):
+     return np.logspace(1,ilosc,num=ilosc, base=liczba)
+ print(generuj(2,4))
 
 #ZAD5
 def generuj(n):
@@ -30,6 +32,28 @@ print(generuj(5))
 
 #ZAD6
 
+malina = np.array(list('malina'))
+mrowka = np.array(list('mrówka'))
+armata = np.array(list('armata'))
+armata = np.flip(armata)
+
+ wykreslanka = np.zeros((6,6), dtype='str')
+
+ print(wykreslanka)
+
+ wykreslanka = np.diag(mrowka)
+ wykreslanka[:, 0] = malina
+ wykreslanka[5,::-1] = armata
+ #wykreslanka[5,:] = armata
+ print(wykreslanka)
+ print("")
+ wykreslanka[:, 0] = mrowka
+ wykreslanka[5,::-1] = armata
+ for a in range(5):
+     wykreslanka[a,a] = malina[a]
+ print(wykreslanka)
+
+
 #ZAD7
 
 def macierz(n):
@@ -41,7 +65,31 @@ def macierz(n):
 print(macierz(5))
 
 #ZAD8
+ def zad8(tab, kierunek='poziomo'):
+     print(tab)
+     if kierunek == 'poziomo':
+         # nieparzysta liczba wierszy
+         if tab.shape[0] % 2 != 0:
+             print("Tablica posiada nieprzystą liczbę wierszy")
+             return
+         p1 = tab[0:int(tab.shape[0]/2), :]
+         p2 = tab[int(tab.shape[0]/2):, :]
+         print("***** część 1 *****")
+         print(p1)
+         print("***** część 2 *****")
+         print(p2)
+     elif kierunek == "pionowo":
+         if tab.shape[1] % 2 != 0:
+             print("Tablica posiada nieprzystą liczbę kolumn")
+             return
+         p1 = tab[:, 0:int(tab.shape[1]/2)]
+         p2 = tab[:, int(tab.shape[1] / 2):]
+         print("***** część 1 *****")
+         print(p1)
+         print("***** część 2 *****")
+         print(p2)
 
+ zad8(np.arange(36).reshape((6,6)), kierunek='pionowo')
 
 #ZAD9
 a = 0
